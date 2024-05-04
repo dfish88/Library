cf3cf7074fea23e1077ad03458b5ec544d7964a2
Danny Fisher <fisher.d88@gmail.com>
2024-05-03 19:45:39 -0600

    Add function to display books

--- library.js
+++ library.js
@@ -1,3 +1,7 @@
+const UI = {
+    booksDiv : document.querySelector('.books')
+};
+
 const myLibrary = [];
 
 function Book(title, author, pages){
@@ -16,3 +20,29 @@ function addBookToLibrary(title, author, pages){
     var newBook = new Book(title, author, pages);
     myLibrary.push(newBook);
 }
+
+function displayBooks(){
+    for (const index in myLibrary){
+
+        let bookCard = document.createElement('div');
+        bookCard.classList.add('bookCard');
+
+        let title = document.createElement('p');
+        title.innerText = myLibrary[index].title;
+        let author = document.createElement('p');
+        author.innerText = myLibrary[index].author;
+        let pages = document.createElement('p');
+        pages.innerText = myLibrary[index].pages;
+
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(pages);
+
+        UI.booksDiv.appendChild(bookCard);
+    }
+}
+
+addBookToLibrary('Book 1', 'Cool Guy', 399);
+addBookToLibrary('Book 2', 'Cool Guy', 399);
+addBookToLibrary('Book 3', 'Cool Guy', 399);
+displayBooks();
